bb6514901153094c187ad041f5011077fc65e154
Aman Kumar <aman228115918@gmail.com>
2024-09-22 11:31:09 +0530

    homescreen left part done

--- src/screens/HomeScreen/index.jsx
+++ src/screens/HomeScreen/index.jsx
@@ -1,3 +1,29 @@
+import { FaFileCode, FaPlus } from "react-icons/fa";
 export const HomeScreen = () => {
-  return <h1>Home Screen</h1>;
+  return (
+    <div className="h-dvh grid grid-cols-[2fr_3fr]  ">
+      {/* left-container */}
+
+      <div className="bg-black text-white flex justify-center items-center">
+        <div className="flex flex-col items-center justify-center gap-3">
+          <FaFileCode className="text-9xl  text-cyan-400" />
+          <h1 className="font-bold">CODE EDITOR</h1>
+          <h2 className="font-semibold text-gray-300 font-serif">Code.Complie.Debug</h2>
+          <button className="font-medium cursor-pointer bg-white rounded-full w-full h-12 p-2 text-black hover:shadow-[2px_2px_6px_gray] transition duration-1000 flex gap-1 justify-center items-center">
+            <span>
+              <FaPlus />
+            </span>
+
+            <span>Create New Playground</span>
+          </button>
+        </div>
+      </div>
+
+      {/* right-container */}
+
+      <div>
+        <h1>right</h1>
+      </div>
+    </div>
+  );
 };
